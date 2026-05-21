d77a97cffaaac907f369c5af886d546935e96333
shahinrahmannaim <50427724+shahinrahmannaim@users.noreply.github.com>
2026-05-21 14:17:04 +0200

    project structure commit

---
+++ src/main/java/com/mmotors/m_motors_backend/MMotorsBackendApplication.java
@@ -0,0 +1,13 @@
+package com.mmotors.m_motors_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MMotorsBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MMotorsBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mmotors/m_motors_backend/MMotorsBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mmotors.m_motors_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MMotorsBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
